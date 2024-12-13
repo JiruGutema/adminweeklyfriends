1e44441004cf11469e03e5756c77a46d7527d230
JiruGutema <jethior1@gmail.com>
2024-12-13 20:23:35 +0300

    admin page

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import WeeklyPairing from "./Components/WeeklyFriends/WeeklyFriends";
+import Home from "./Components/Home/Home";
+
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home apiUrl="https://web-weekly-friends-generator.onrender.com" />
+            }
+          />
+          <Route
+            path="/admin"
+            element={
+              <WeeklyPairing apiUrl="https://web-weekly-friends-generator.onrender.com" />
+            }
+          />
+        </Routes>
+      </Router>
+      <div className="maintanier">
+        Developed by:{" "}
+        <span>
+          {" "}
+          <a href="https://t.me/@jethior">Systemd</a>
+        </span>{" "}
+      </div>
+    </>
+  );
+}
+
+export default App;
